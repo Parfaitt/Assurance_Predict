727dff0e98c513de029b65168865552d5d602754
Parfaitt <ngoran.parfait42@gmail.com>
2023-05-20 15:49:41 +0000

    2nd commit

--- Medical_Insurance_Cost_Prediction_Web_App.py
+++ Medical_Insurance_Cost_Prediction_Web_App.py
@@ -10,7 +10,6 @@ import numpy as np
 import pickle
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 # loading the saved model
